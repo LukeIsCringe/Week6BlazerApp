923a6949073177bbd037b7e4db21ae0f6603c883
bi54ul <bi54ul@FOT-CELLA23>
2024-10-31 10:42:33 +0000

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Week6BlazerApp.Data.Models;
+
+namespace Week6BlazerApp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<Module> Modules { get; set; }
+    }
+}
